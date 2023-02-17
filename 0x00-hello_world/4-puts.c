fae2b69d4477ed9e96068d986584e1db11034ecd
blvcko23 <254.lethalle@gmail.com>
2023-02-17 09:07:05 -0800

    wueeh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 /**
- *
  * main - A programm that prints a line with puts function
- *
- * Return:  0 (sccessful)
- *
+ * Return:  0 (success)
  * int main(void)
- *
  /
  int main(void)
-
  {
  
  puts("\"Prpgramming is like building a multilingual puzzle");
-
  Return (0);
 
  }
