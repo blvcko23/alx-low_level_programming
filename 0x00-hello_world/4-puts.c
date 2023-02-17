6d968030dae644e2382fcf5be1c61a5a1e046077
blvcko23 <254.lethalle@gmail.com>
2023-02-17 09:36:36 -0800

    ngoori

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  * Return: Always 0 (success)
- */
+ **/
 
- int main(void)
- {
- puts("\"Prpgramming is like building a multilingual puzzle");
- Return (0);
- }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return (0);
+}
