18c7162f7b0a1670b91e5fbc711ea183e1633ba8
blvcko23 <254.lethalle@gmail.com>
2023-11-21 15:11:23 -0800

    CDI argument count & vectors

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,47 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
 
 /**
- * main - prints minimum number coins to make change for an amount of money.
- * @argc: number of arguments
- * @argv: array with the arguments
- *
- * Return: always 0
- **/
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
 
 int main(int argc, char *argv[])
 {
-	int dev = 0, coins = 0;
-	char c[] = "Error";
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc != 2)
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
-		printf("%s\n", c);
-		return (1);
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
 	}
-	dev = atoi(argv[1]);
-	while (dev >= 25)
-	{
-		dev -= 25;
-		coins++;
-	}
-	while (dev >= 10)
-	{
-		dev -= 10;
-		coins++;
-	}
-	while (dev >= 5)
-	{
-		dev -= 5;
-		coins++;
-	}
-	while (dev >= 2)
-	{
-		dev -= 2;
-		coins++;
-	}
-	if (dev == 1)
-		coins++;
-	printf("%d\n", coins);
 	return (0);
 }
