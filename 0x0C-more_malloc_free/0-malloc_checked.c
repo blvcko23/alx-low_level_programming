393243a834340a55b52d567bfb14b2a3940dbac6
blvcko23 <254.lethalle@gmail.com>
2023-12-11 13:01:36 -0800

    Noma code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *g = malloc(b);
+
+	if (g == NULL)
+	{
+		exit(98);
+	}
+	return (g);
+}
